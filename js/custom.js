9c5964fca804c30dc6fed86548c2e54b23a53e74
unknown <ansari.anis37@gmail.com>
2022-11-13 22:41:23 +0530

    first commit

---
+++ js/custom.js
@@ -0,0 +1,56 @@
+(function ($) {
+  "use strict";
+
+  // COUNTER NUMBERS
+  jQuery(".counter-thumb").appear(function () {
+    jQuery(".counter-number").countTo();
+  });
+
+  // CUSTOM LINK
+  $(".smoothscroll").click(function () {
+    var el = $(this).attr("href");
+    var elWrapped = $(el);
+    var header_height = $(".navbar").height();
+
+    scrollToDiv(elWrapped, header_height);
+    return false;
+
+    function scrollToDiv(element, navheight) {
+      var offset = element.offset();
+      var offsetTop = offset.top;
+      var totalScroll = offsetTop - navheight;
+
+      $("body,html").animate(
+        {
+          scrollTop: totalScroll,
+        },
+        300
+      );
+    }
+  });
+})(window.jQuery);
+
+// ========gallery=========
+// $('.portfolio-item').isotope({
+//  	itemSelector: '.item',
+//  	layoutMode: 'fitRows'
+//  });
+$(".portfolio-menu ul li").click(function () {
+  $(".portfolio-menu ul li").removeClass("active");
+  $(this).addClass("active");
+
+  var selector = $(this).attr("data-filter");
+  $(".portfolio-item").isotope({
+    filter: selector,
+  });
+  return false;
+});
+$(document).ready(function () {
+  var popup_btn = $(".popup-btn");
+  popup_btn.magnificPopup({
+    type: "image",
+    gallery: {
+      enabled: true,
+    },
+  });
+});
